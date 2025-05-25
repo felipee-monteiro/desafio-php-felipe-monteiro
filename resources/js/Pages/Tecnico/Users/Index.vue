a3c1d42e24223a241dda13d0293fdcfe34b2bc6d
felipe <fm3209742@gmail.com>
2025-05-25 20:57:02 -0300

    feat: users managment ui and email validation ui

--- resources/js/Pages/Tecnico/Users/Index.vue
+++ resources/js/Pages/Tecnico/Users/Index.vue
@@ -1,83 +1,170 @@
 <template>
-    <CrudLayout
-        mainTitle="Usuários"
-        notFoundMessage="Nenhum usuário encontrado"
-        :data="users"
-    >
-        <template #default="{ data }">
-            <div class="flex justify-between items-center">
-                <div>
-                    <h2 class="text-lg font-semibold text-gray-800">
-                        {{ data.name }}
-                    </h2>
-                    <p class="text-sm text-gray-500">
-                        Criado em: {{ data.created_at }} | Atualizado em:
-                        {{ data.updated_at }} | Role: {{ data.role.name }}
-                    </p>
-                </div>
-                <div class="flex gap-2">
-                    <Dialog
-                        :title="`Editar Categoria - ${data.name}`"
-                        decription="Edite a categoria dos chamados"
-                    >
-                        <template #trigger>
-                            <button
-                                class="text-sm text-blue-600 hover:underline"
-                            >
-                                Editar
-                            </button>
-                        </template>
-                        <template #default>
-                            <div class="my-2">
-                                <form
-                                    id="edit.submit"
-                                    @submit.prevent="handleEdit(data)"
+    <AppLayout>
+        <CrudLayout
+            mainTitle="Usuários"
+            notFoundMessage="Nenhum usuário encontrado"
+            :data="users"
+        >
+            <template #default="{ data }">
+                <div class="flex justify-between items-center">
+                    <div>
+                        <h2 class="text-lg font-semibold text-gray-800">
+                            {{ data.name }}
+                        </h2>
+                        <p class="text-sm text-gray-500">
+                            Criado em: {{ data.created_at }} | Atualizado em:
+                            {{ data.updated_at }} | Role: {{ data.role.name }}
+                        </p>
+                    </div>
+                    <div class="flex gap-2">
+                        <Dialog
+                            :title="`Editar Usuário - ${data.name}`"
+                            decription="Controle as configurações de acesso deste usuário"
+                        >
+                            <template #trigger>
+                                <button
+                                    class="text-sm text-blue-600 hover:underline"
                                 >
-                                    <Label for="category_name">Nome</Label>
-                                    <Input
-                                        v-model="editData.newName"
-                                        id="category_name"
-                                    />
-                                </form>
-                            </div>
-                        </template>
-                        <template #footer>
-                            <button
-                                type="submit"
-                                form="edit.submit"
-                                class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded shadow"
-                            >
-                                Salvar
-                            </button>
-                        </template>
-                    </Dialog>
+                                    Editar
+                                </button>
+                            </template>
+                            <template #default>
+                                <div class="my-2">
+                                    <div class="space-y-4">
+                                        <FormField
+                                            v-model="editData.isActive"
+                                            name="isActive"
+                                        >
+                                            <FormItem
+                                                class="flex flex-row items-center justify-between rounded-lg border p-4"
+                                            >
+                                                <div class="space-y-0.5">
+                                                    <FormLabel
+                                                        class="text-base"
+                                                    >
+                                                        Permitir acesso
+                                                    </FormLabel>
+                                                    <FormDescription>
+                                                        Permitir que este
+                                                        usuário acesse a
+                                                        plataforma
+                                                        <br />com sua role atual
+                                                    </FormDescription>
+                                                </div>
+                                                <FormControl>
+                                                    <Switch
+                                                        :v-model="
+                                                            editData.isActive
+                                                        "
+                                                        @update="void 0"
+                                                    />
+                                                </FormControl>
+                                            </FormItem>
+                                        </FormField>
+                                        <FormField
+                                            v-model="editData.role_id"
+                                            name="role_id"
+                                        >
+                                            <FormItem
+                                                class="flex flex-col gap-2 items-center justify-between rounded-lg border p-4"
+                                            >
+                                                <div class="space-y-0.5">
+                                                    <FormLabel
+                                                        class="text-base"
+                                                    >
+                                                        Role
+                                                    </FormLabel>
+                                                    <FormDescription>
+                                                        Mecanismo que dá acessos
+                                                        especiais à áreas
+                                                        exclusivas do sistema,
+                                                        correspondente ao papel
+                                                        do usuário na vida real.
+                                                    </FormDescription>
+                                                </div>
+                                                <FormControl>
+                                                    <Select>
+                                                        <SelectTrigger>
+                                                            <SelectValue
+                                                                :placeholder="
+                                                                    data.role
+                                                                        .name
+                                                                "
+                                                            />
+                                                        </SelectTrigger>
+                                                        <SelectContent>
+                                                            <SelectGroup>
+                                                                <SelectLabel
+                                                                    >Roles</SelectLabel
+                                                                >
+                                                                <SelectItem
+                                                                    :value="
+                                                                        data
+                                                                            .role
+                                                                            .id
+                                                                    "
+                                                                >
+                                                                    {{
+                                                                        data
+                                                                            .role
+                                                                            .name
+                                                                    }}
+                                                                </SelectItem>
+                                                            </SelectGroup>
+                                                        </SelectContent>
+                                                    </Select>
+                                                </FormControl>
+                                            </FormItem>
+                                        </FormField>
+                                    </div>
+                                </div>
+                            </template>
+                            <template #footer>
+                                <button
+                                    type="submit"
+                                    form="edit.submit"
+                                    class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded shadow"
+                                >
+                                    Salvar
+                                </button>
+                            </template>
+                        </Dialog>
+                    </div>
                 </div>
-            </div>
-        </template>
-    </CrudLayout>
+            </template>
+        </CrudLayout>
+    </AppLayout>
 </template>
 
 <script setup>
 import Dialog from "@/Components/Dialog.vue";
-import { Input } from "@/Components/ui/input";
-import { Label } from "@/Components/ui/label";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/Components/ui/select";
 import CrudLayout from "@/Layouts/CrudLayout.vue";
-import { rmStringSpace } from "@/utils";
+import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+} from "@/Components/ui/form";
+import { Switch } from "@/Components/ui/switch";
 import useForm from "@/Composables/form.js";
+import AppLayout from "@/Layouts/AppLayout.vue";
 
 const editData = useForm({
-    newName: "",
-});
-
-const createData = useForm({
-    name: "",
+    role_id: 0,
+    isActive: true,
 });
 
 function handleEdit(data) {
-    if (rmStringSpace(editData.newName) === rmStringSpace(data.name)) {
-        return alert("Por favor, escolha um nome diferente");
-    }
-
     editData.put(`/tecnico/users/${data.id}`);
 }
 
