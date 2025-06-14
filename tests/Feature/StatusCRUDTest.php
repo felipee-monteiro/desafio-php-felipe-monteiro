a6b8968d764e6de5aa631d53f413fe9eaafbc2eb
felipe <fm3209742@gmail.com>
2025-06-13 21:03:40 -0300

    feat(tests): improve additional tests on CRUD resources

--- tests/Feature/StatusCRUDTest.php
+++ tests/Feature/StatusCRUDTest.php
@@ -4,17 +4,67 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\TestCase;
 
 final class StatusCRUDTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function testExample(): void
-    {
-        $response = $this->get('/');
+    public string $routePrefix = 'tecnico/chamados';
 
-        $response->assertStatus(200);
+    public static function getRoutesProvider(): array
+    {
+        return [
+            ['status/manage'],
+        ];
+    }
+
+    public static function getProtectedRoutesProvider(): array
+    {
+        return [
+            [[
+                'route'  => 'status/manage',
+                'method' => 'post',
+            ]],
+            [[
+                'route'  => 'status/manage',
+                'method' => 'post',
+            ]],
+            [[
+                'route'  => 'status/manage/1',
+                'method' => 'put',
+            ]],
+            [[
+                'route'  => 'status/manage/1',
+                'method' => 'delete',
+            ]],
+        ];
+    }
+
+    #[DataProvider('getRoutesProvider')]
+    public function testExample(string $route): void
+    {
+        $user = User::factory()->create([
+            'role_id'   => 2,
+            'is_active' => true,
+        ]);
+        $response = $this->actingAs($user)->get($this->routePrefix . "/{$route}");
+
+        $response->assertOk();
+    }
+
+    #[DataProvider('getProtectedRoutesProvider')]
+    public function testShouldNotAllowMeToAccessRoutesWithColaboratorRole(array $routeMetadata): void
+    {
+        \extract($routeMetadata);
+
+        $user = User::factory()->create([
+            'role_id'   => 1,
+            'is_active' => true,
+        ]);
+
+        $response = $this->actingAs($user)->{$method}($this->routePrefix . "/{$route}");
+
+        $response->assertForbidden();
     }
 }
